094fdd4db7734508ecad2e9009bc97a234de8d2c
Mohammed Nokri <mohammed.nokri@itg.co.uk>
2020-04-13 17:02:47 +0100

    Applied useFocus

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,13 +1,18 @@
 import React, { useRef } from 'react';
 import { TaskItem } from '../TaskItem';
-import { useTodoList } from './hooks'
+import { useTodoList } from './hooks';
+import { UseFocus } from '../Hooks/UseFocus';
+
+import './styles/todoList.css';
 
 const TodoList = () => {
     const inputRef = useRef();
     const { getText, context, onSubmitHandler, onChangeHandler} = useTodoList('');
+    UseFocus();
 
     return (
-        <>
+        <div className="todo-list__container wrapper">
+            <h1>Checklist</h1>
             <form onSubmit={onSubmitHandler}>
                 <input
                     type="text"
@@ -15,14 +20,12 @@ const TodoList = () => {
                     value={getText}
                     onChange={onChangeHandler}
                 />
-                <br />
-                { context.tasks.map((item, idx) => {
-                    return (
-                        <TaskItem idx={idx} key={idx} item={item} />
-                    );
-                })}
             </form>
-        </>
+
+            { context.tasks.map((item, idx) => 
+                <TaskItem idx={idx} key={idx} item={item} />
+            )}
+        </div>
     );
 };
 
